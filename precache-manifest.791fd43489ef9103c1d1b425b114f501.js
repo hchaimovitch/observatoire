7aabcd016845feeda0c86ad6522292ddce2982ea
xofni <fakenewsProject@hotmail.com>
2018-10-05 17:29:24 +0200

    Updates

---
+++ precache-manifest.791fd43489ef9103c1d1b425b114f501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jeu/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "05a47949cceb4e0ec750",
+    "url": "/jeu/static/js/runtime~main.05a47949.js"
+  },
+  {
+    "revision": "7796f25f288cf5b9a61c",
+    "url": "/jeu/static/js/main.7796f25f.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/jeu/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "7796f25f288cf5b9a61c",
+    "url": "/jeu/static/css/main.05079912.chunk.css"
+  },
+  {
+    "revision": "6252bb42195c754b92b7b2b9a749d114",
+    "url": "/jeu/index.html"
+  }
+];
