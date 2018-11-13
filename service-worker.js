8b53f2e2ca0a02136d8b111a0c7752b1db5b07de
xofni <fakenewsProject@hotmail.com>
2018-11-13 17:30:51 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/jeu/precache-manifest.2d5e5080f40b9795093f69167984ddd6.js"
+  "/observatoire/precache-manifest.22a3e11b8e5693b1041452dc6008f729.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/jeu/index.html", {
+workbox.routing.registerNavigationRoute("/observatoire/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
